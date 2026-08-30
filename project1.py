d841c6bc3dca4cf44abf673fcca6df2791309c85
vihaangoyal21-cpu <vihaangoyal21@gmail.com>
2026-08-30 21:47:13 +0530

    Updated Commit

---
+++ project1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the length - "))
+b = int(input("Enter the breadth - "))
+c = int(input("Enter the height - "))
+Area = a*b
+Volume = a*b*c 
+if a==b==c:    
+   print("The area of this cube is ", Area ,"and the volume of this cube is ", Volume)
+else : 
+   print("The area of this cuboid is", Area ,"and the volume of this cuboid is ", Volume)
+
+
