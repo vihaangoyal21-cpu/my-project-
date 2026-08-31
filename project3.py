2fae8afade4fcaff199d5ecfffe0d4c5e50cf256
vihaangoyal21-cpu <vihaangoyal21@gmail.com>
2026-09-01 04:53:55 +0530

    Updated Commit

--- project3.py
+++ project3.py
@@ -1,15 +1,28 @@
 import random 
-a = random.randint(1,100) 
-b = int(input("I am guessing a number between 1 & 100 , try and guess it - "))
-
 while True :
- if a > b : 
-  b = int(input("Too low! Try again - "))
+ d = input("Choose difficulty(Easy/Medium/Hard) - ")
+ if d.strip().lower() == "easy" :
+   max = 50 
+ elif d.strip().lower() == "medium" :
+   max = 100
+ elif d.strip().lower() == "hard" :
+   max = 200
+ a = random.randint(1,max) 
+ b = int(input(f"I am guessing a number between 1 & {max} try and guess it - "))
+ while True :
+  if a > b : 
+   b = int(input("Too low! Try again - "))
 
- elif a < b : 
-  b = int(input("Too high! Try again - "))
-
- if a==b :
+  elif a < b : 
+   b = int(input("Too high! Try again - "))
+ 
+  if a==b :
    break 
-print("You got it !")
+ print("You got it !")
+ c = input("Do you want to try again ? - ")
+ if c.strip().lower() == "no" :   
+  break 
+
+
+ 
   
