8cf0314df71052c1d12b17493623bec69063a8f7
vihaangoyal21-cpu <vihaangoyal21@gmail.com>
2026-09-01 17:11:03 +0530

    Updated Commit

--- project3.py
+++ project3.py
@@ -1,27 +1,27 @@
 import random 
 while True :
- d = input("Choose difficulty(Easy/Medium/Hard) - ")
- if d.strip().lower() == "easy" :
+  d = input("Choose difficulty(Easy/Medium/Hard) - ")
+  if d.strip().lower() == "easy" :
    max = 50 
- elif d.strip().lower() == "medium" :
+  elif d.strip().lower() == "medium" :
    max = 100
- elif d.strip().lower() == "hard" :
+  elif d.strip().lower() == "hard" :
    max = 200
- a = random.randint(1,max) 
- b = int(input(f"I am guessing a number between 1 & {max} try and guess it - "))
- while True :
-  if a > b : 
-   b = int(input("Too low! Try again - "))
+  a = random.randint(1,max) 
+  b = int(input(f"I am guessing a number between 1 & {max} try and guess it - "))
+  while True :
+    if a > b : 
+     b = int(input("Too low! Try again - "))
 
-  elif a < b : 
-   b = int(input("Too high! Try again - "))
+    elif a < b : 
+      b = int(input("Too high! Try again - "))
  
-  if a==b :
-   break 
- print("You got it !")
- c = input("Do you want to try again ? - ")
- if c.strip().lower() == "no" :   
-  break 
+    if a==b :
+      break 
+  print("You got it !")
+  c = input("Do you want to try again ? - ")
+  if c.strip().lower() == "no" :   
+    break 
 
 
  
