e8987784f8969ea8a7ba7eae21352141775f6dc9
vihaangoyal21-cpu <vihaangoyal21@gmail.com>
2026-09-01 17:38:36 +0530

    Updated Commit

--- project4.py
+++ project4.py
@@ -1,15 +1,19 @@
 while True : 
- a = int(input("Please enter the number - "))
- b = int(input("Please enter the number with which you want to check the divisibility - "))
+    
+    a = int(input("Please enter the number - "))
+    b = int(input("Please enter the number with which you want to check the divisibility - "))
+    
 
- if b==0 :
-  print("You cannot check the divisibilty by 0")
- elif a % b == 0 :
-  print(f"The given number is divisble by {b}")
- else :
-  print(f"The given number is not divisble by {b}")
+    if b==0 :
+     print("You cannot check the divisibilty by 0")
+     
+    elif a % b == 0 :
+     print(f"The given number is divisble by {b}")
+     
+    else :
+     print(f"The given number is not divisble by {b}")
 
- c = input("Do you want to continue - ")
+    c = input("Do you want to continue - ")
 
- if c.strip().lower() == "no" :
-  break 
+    if c.strip().lower() == "no" :
+     break 
