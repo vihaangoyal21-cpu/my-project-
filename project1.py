735cc957e95f8dd9a52b35c5b256847fc74ba53b
vihaangoyal21-cpu <vihaangoyal21@gmail.com>
2026-09-01 17:32:38 +0530

    Updated Commit

--- project1.py
+++ project1.py
@@ -1,11 +1,27 @@
-a = int(input("Enter the length - "))
-b = int(input("Enter the breadth - "))
-c = int(input("Enter the height - "))
-Area = a*b
-Volume = a*b*c 
-if a==b==c:    
-   print("The area of this cube is ", Area ,"and the volume of this cube is ", Volume)
-else : 
-   print("The area of this cuboid is", Area ,"and the volume of this cuboid is ", Volume)
-
-
+while True : 
+ t = input("Enter the type of figure you want(2D/3D) - ")
+ if t.strip().lower() == "2d" : 
+  a = input("Choose the shape/figure - ")
+  if a.strip().lower() == "circle" : 
+   r = float(input("Enter the radius of the circle - "))
+   from math import pi  
+   print(f"The circumference of this circle is {2*pi*r} and its area is {pi*(r**2)}")
+ elif a.strip().lower() == "square" :
+  b = float(input("Enter the length of the side - "))
+  print(f"The perimeter of this square is {4*b} and its area is {b**2}")
+ elif a.strip().lower() == "rectangle" :
+  l = float(input("Enter the length of the rectangle - "))
+  b = float(input("Enter the breadth of the rectangle - "))
+  print(f"The perimeter of this rectangle is {2*(a + b)} and its area is {a*b}")
+ elif a.strip().lower() == "triangle" :
+  a = float(input("Enter the length of a side of the triangle - "))
+  b = float(input("Enter the length of the other side of the triangle - "))
+  c = float(input("Enter the length of another side of the triangle - "))
+  s = (a + b + c)/2 
+  Area = ((s(s-a)(s-b)(s-c)))**(1/2)
+ if a==b and b==c :
+     print(f"The area of this equilateral triangle is {Area}")
+ elif a==b!=c or a!=b==c or a==c!=b : 
+     print(f"The area of this isosceles triangle is {Area}")
+ elif a!=b!=c : 
+     print(f"The area of this scalene triangle is {Area}")
